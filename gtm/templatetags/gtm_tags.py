6daa78b7f50b9ff3772c0fb01111c19d459d9483
Bennett Rogers <bennettrogers@gmail.com>
2019-01-23 14:14:04 -0800

    Adding params for GTM Environments
    - E.g. https://www.napkyn.com/2017/07/12/ga-gtm-environments/

--- gtm/templatetags/gtm_tags.py
+++ gtm/templatetags/gtm_tags.py
@@ -5,7 +5,7 @@ register = Library()
 ri = register.inclusion_tag
 
 
-def gtm_tag(context, google_tag_id=None):
+def gtm_tag(context, google_tag_id=None, google_tag_auth=None, google_tag_preview=None):
     request = context.get("request")
     if request:
         if request.COOKIES.get(getattr(settings, "GOOGLE_TAG_BYPASS_COOKIE", None)):
@@ -13,7 +13,15 @@ def gtm_tag(context, google_tag_id=None):
 
     if google_tag_id is None:
         google_tag_id = getattr(settings, "GOOGLE_TAG_ID", None)
-    return {"google_tag_id": google_tag_id}
+    if google_tag_auth is None:
+        google_tag_auth = getattr(settings, "GOOGLE_TAG_AUTH", None)
+    if google_tag_preview is None:
+        google_tag_preview = getattr(settings, "GOOGLE_TAG_PREVIEW", None)
+    return {
+        "google_tag_id": google_tag_id,
+        "google_tag_auth": google_tag_auth,
+        "google_tag_preview": google_tag_preview,
+    }
 
 
 ri("gtm/gtm.html", name="gtm", takes_context=True)(gtm_tag)
